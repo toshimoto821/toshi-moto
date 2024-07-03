4f7055789fee3e66ea8f00297b952870cfe49d9e
Toshi Moto <toshimoto821@proton.me>
2024-07-03 06:59:53 -0400

    fix: loading

--- apps/web-ui/tailwind.config.js
+++ apps/web-ui/tailwind.config.js
@@ -40,7 +40,7 @@ export default {
       animation: {
         slideDown: "slideDown 300ms cubic-bezier(0.87, 0, 0.13, 1)",
         slideUp: "slideUp 300ms cubic-bezier(0.87, 0, 0.13, 1)",
-        fadeOut: "fadeOut 1s ease-in-out",
+        fadeOut: "fadeOut 3s ease-in-out",
       },
     },
   },
