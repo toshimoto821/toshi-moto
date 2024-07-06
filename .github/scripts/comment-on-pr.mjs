9f3bbeb75852e3a06c273c20e34b0cb8c6393ec7
Toshi Moto <toshimoto821@proton.me>
2024-07-06 13:27:43 -0400

    fix: issue number

--- .github/scripts/comment-on-pr.mjs
+++ .github/scripts/comment-on-pr.mjs
@@ -1,15 +1,19 @@
 // .github/scripts/comment-on-pr.mjs
 import { getOctokit, context } from "@actions/github";
-import * as core from "@actions/core";
+// @todo either use or remove dep
+//import * as core from "@actions/core";
 
 const githubToken = process.env.GITHUB_TOKEN;
 const octokit = getOctokit(githubToken);
+const issue_number = process.env.PULL_REQUEST_NUMBER;
 
 const issueComment = context.issue;
+console.log(JSON.stringify(issueComment));
 const commentBody =
   process.env.COMMENT_BODY || "Automated comment from GitHub Actions workflow.";
 
 await octokit.rest.issues.createComment({
   ...issueComment,
   body: commentBody,
+  issue_number,
 });
