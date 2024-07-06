f32b61bb85bc1fd980ec135a71d9d106ffb86388
Toshi Moto <toshimoto821@proton.me>
2024-07-06 11:13:12 -0400

    fix: ci issues

--- apps/web-ui/postcss.config.js
+++ apps/web-ui/postcss.config.js
@@ -1,7 +1,12 @@
+// eslint-disable-next-line
+const isCI = process.env.CYPRESS_RUNNING === "true";
+
 export default {
   plugins: {
     tailwindcss: {
-      config: "./apps/web-ui/tailwind.config.js",
+      config: isCI
+        ? "./apps/web-ui/tailwind.config.js"
+        : "./tailwind.config.js",
     },
     autoprefixer: {},
   },
