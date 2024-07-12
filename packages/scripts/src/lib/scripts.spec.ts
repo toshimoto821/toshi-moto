0f48046e850b4a7cd5deeb0a818c33bb3c1a4394
Toshi Moto <toshimoto821@proton.me>
2024-07-11 20:02:36 -0400

    fix: changelog table

--- packages/scripts/src/lib/scripts.spec.ts
+++ packages/scripts/src/lib/scripts.spec.ts
@@ -1,8 +1,15 @@
-import { scripts } from "./scripts";
+// import { scripts } from "./scripts";
+import { createMarkdownTable } from "./util/injectImagesToChangelog";
 // import { getTag } from "./util/injectImagesToChangelog";
+
 describe("scripts", () => {
-  it("should work", () => {
-    expect(scripts()).toEqual("scripts");
+  it("markdown table", () => {
+    const result = createMarkdownTable(
+      ["one", "two", "three", "four", "five"],
+      "hash",
+      3
+    );
+    expect(result.length).toEqual(618);
   });
   // it("should get tags", async () => {
   //   const tag = await getTag("a717bc832119d244ab669c909f428abc40d44cef");
