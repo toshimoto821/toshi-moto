4a205b3caf6e84d1719123f1abcc0ce8c59b42bc
Toshi Moto <toshimoto821@proton.me>
2025-03-27 07:23:11 -0400

    chore: lint

--- packages/scripts/src/lib/scripts.spec.ts
+++ packages/scripts/src/lib/scripts.spec.ts
@@ -3,11 +3,13 @@ import { createMarkdownTable } from "./util/injectImagesToChangelog";
 // import { getTag } from "./util/injectImagesToChangelog";
 
 describe("scripts", () => {
-  it("markdown table", () => {
+  test("markdown table", () => {
     const result = createMarkdownTable(
-      ["https://api.webshotarchive.com/api/image/id/86d2e951-eaf5-4008-853c-bc9827945273.png", 
-      "https://api.webshotarchive.com/api/image/id/86d2e951-eaf5-4008-853c-bc9827945273.png", 
-      "https://api.webshotarchive.com/api/image/id/86d2e951-eaf5-4008-853c-bc9827945273.png"],
+      [
+        "https://api.webshotarchive.com/api/image/id/86d2e951-eaf5-4008-853c-bc9827945273.png",
+        "https://api.webshotarchive.com/api/image/id/86d2e951-eaf5-4008-853c-bc9827945273.png",
+        "https://api.webshotarchive.com/api/image/id/86d2e951-eaf5-4008-853c-bc9827945273.png",
+      ],
       "hash",
       3
     );
