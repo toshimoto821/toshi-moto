c076c1b3d6c7b3f6e8bf1036c5353157f72393d5
Toshi Moto <toshimoto821@proton.me>
2025-03-28 19:09:46 -0400

    chore: e2e

--- apps/web-ui/src/main.tsx
+++ apps/web-ui/src/main.tsx
@@ -13,6 +13,7 @@ if (window.location.pathname !== "/") {
   window.location.href = "/";
 }
 
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <AppProvider>
