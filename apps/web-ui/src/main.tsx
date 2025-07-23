efeba9481ab8822bfbbf5f5809c4ccc129d95cef
Toshi Moto <toshimoto821@proton.me>
2025-07-23 09:03:38 -0400

    fix: bump

--- apps/web-ui/src/main.tsx
+++ apps/web-ui/src/main.tsx
@@ -12,7 +12,7 @@ console.log(`Version: __VERSION__`);
 if (window.location.pathname !== "/") {
   window.location.href = "/";
 }
-
+// build
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
