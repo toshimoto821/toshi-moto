d4eb7444bc0c307ed9b6570d674d87c4ef883719
Toshi Moto <toshimoto821@proton.me>
2025-08-02 13:51:58 -0400

    fix: hide-price

--- apps/web-ui/src/lib/hooks/useNumberObfuscation.ts
+++ apps/web-ui/src/lib/hooks/useNumberObfuscation.ts
@@ -1,12 +1,16 @@
 import { useAppSelector } from "./store.hooks";
 import { selectPrivatePrice } from "../slices/ui.slice";
+import { useCallback } from "react";
 
 export const useNumberObfuscation = () => {
   const privatePrice = useAppSelector(selectPrivatePrice);
-  return (price: string | number) => {
-    if (privatePrice) {
-      return "••.•••";
-    }
-    return price;
-  };
+  return useCallback(
+    (price: string | number) => {
+      if (privatePrice) {
+        return "••.•••";
+      }
+      return price;
+    },
+    [privatePrice]
+  );
 };
