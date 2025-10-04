fe6aa6ecf5b1b884a24da5229d58c8753baeb5c5
Toshi Moto <toshimoto821@proton.me>
2025-10-04 07:28:24 -0400

    fix: loading

--- apps/web-ui/src/components/loading/WaitForData.tsx
+++ apps/web-ui/src/components/loading/WaitForData.tsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react";
 import { Loading } from "./Loading";
 
 const MAX_TRY_COUNT = 60;
-const WAIT_TIME = 2000;
+const WAIT_TIME = 3000;
 
 export const WaitForData = ({ children }: { children: React.ReactNode }) => {
   const [tryCount, setTryCount] = useState(0);
@@ -18,9 +18,11 @@ export const WaitForData = ({ children }: { children: React.ReactNode }) => {
   }, [tryCount, data?.status]);
 
   if (!data?.status && tryCount < MAX_TRY_COUNT) {
-    if (error) {
+    // dont want to flicker between loading and setting up...
+    // so if more than 2 tries, just assume it's setting up
+    if (error || tryCount > 2) {
       return (
-        <Loading message="Setting up your app, this may take a few minutes..." />
+        <Loading message="Setting up Toshi Moto, this may take a few minutes..." />
       );
     }
     return <Loading message="Loading..." />;
